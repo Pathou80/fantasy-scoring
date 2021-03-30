6071bb6d4b9e07d8648a12353d4fdedd09a8cc0a
Pathou80 <79732928+Pathou80@users.noreply.github.com>
2021-03-29 22:20:09 -0400

    correct

--- scoring.js
+++ scoring.js
@@ -1,11 +1,31 @@
-const calculateScore = (player) => {
-    if(player.position === 'QB') {
-        return (player.stats.passing.yards / 25) + (player.stats.passing.touchdowns * 6) +
-               (player.stats.rushing.yards / 10) + (player.stats.touchdowns * - fumbles * 3) +
-               score + passing + rushing
-            
-    }
-} 
+function calculatepassingScore(player) {
+    const yards = player.stats.passing.yards / 25
+    const touchdowns = player.stats.passing.touchdowns * 6
+    const interceptions = player.stats.passing.interceptions * -3
 
+    return yards + touchdowns + interceptions
+}
 
-module.exports = calculateScore 
+function calculateRushingScore(player) {
+    const yards = player.stats.rushing.yards / 10
+    const touchdowns = player.stats.rushing.touchdowns * 6
+    const fumbles = player.stats.rushing.fumbles * -3
+
+    return yards + touchdowns + fumbles
+}
+
+function calculateScore(player){
+    switch (player.position) {
+        case "QB":
+        return calculatepassingScore + calculateRushingScore
+        case "RB":
+        return 
+        case "WR":
+        return 
+        case "TE":
+        return
+        default:
+        return 0
+     }
+}
+module.exports = calculateScore
